ed5fb2a2e4bd599075535ec0b5c2d629b1cb30e8
amaralc <amaral.calil@gmail.com>
2020-07-20 18:26:30 -0300

    FEATURE / Add styles and animation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import api from '../../services/api';
 import { Container, Form, SubmitButton } from './styles';
 
@@ -7,6 +7,7 @@ export default class Home extends Component {
   state = {
     newRepo: '',
     repositories: [],
+    loading: 0,
   };
 
   handleInputChange = (e) => {
@@ -15,6 +16,8 @@ export default class Home extends Component {
 
   handleSubmit = async (e) => {
     e.preventDefault();
+
+    this.setState({ loading: true });
 
     const { newRepo, repositories } = this.state;
 
@@ -27,12 +30,13 @@ export default class Home extends Component {
     this.setState({
       repositories: [...repositories, data],
       newRepo: '',
+      loading: false,
     });
     console.log(data);
   };
 
   render() {
-    const { newRepo } = this.state;
+    const { newRepo, loading } = this.state;
 
     return (
       <Container>
@@ -47,8 +51,12 @@ export default class Home extends Component {
             value={newRepo}
             onChange={this.handleInputChange}
           />
-          <SubmitButton>
-            <FaPlus color="#FFF" size={14} />
+          <SubmitButton loading={loading ? 1 : 0}>
+            {loading ? (
+              <FaSpinner color="#fff" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
           </SubmitButton>
         </Form>
       </Container>
